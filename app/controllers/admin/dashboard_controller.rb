090e3946249c76c12958f27cf0d818fcd6477a6d
bkcatton <ben.k.catton@gmail.com>
2022-03-09 14:24:36 -0800

    fix where environment variable stored

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV[USERNAME], password: ENV[PASSWORD]
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
   def show
+    @count = Product.count
+    # @catCount = Categories.count
   end
+
 end
